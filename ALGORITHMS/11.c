73c02140c33004d53b7355034bbde8881293b02f
ChoiWheatley <chltmdgus604@naver.com>
2020-08-08 16:49:47 +0900

    <codeground>11.c 개구리 뛰기 <실패함>
    여러가지 경우의 수에 대해서는 확실하게 맞는데 시간초과가 발생한다.
    k가 10^9나 되어서 발생한 문제에 대해서는 확실하게 고쳤는데, 이상하게 n에
    대한 for문을 돌리고 나서도 시간초과가 발생한다고 한다. 예외를 더 찾지
    못해 문제풀이를 종료한다.

--- ALGORITHMS/11.c
+++ ALGORITHMS/11.c
@@ -12,18 +12,18 @@
 또한 위의 예에서, K=2 로 주어진다면 개구리는 마지막 돌로 이동할 수가 없다.
 왜냐하면, 개구리가 '좌표 2'의 돌로 이동한 후 '좌표 5'이상의 돌로는 이동할 수 없기 때문이다. 
 
-입력 파일에는 여러 개의 테스트 케이스가 포함될 수 있다.
+입력 파일에는 여 개의 테스트 케이스가 포함될 수 있다.
 파일의 첫째 줄에는 테스트 케이스 개수를 나타내는 자연수 T가 주어지고,
 이후 차례로 T개의 테스트 케이스가 주어진다. ( 1≤T≤5 ) 
 각각의 테스트 케이스 첫 번째 줄에는 '좌표 0'에 놓인 돌을 제외한 나머지 돌들의 개수 N 이 주어진다. ( 1≤N≤1,000,000 )
-두 번째 줄에는 돌들이 놓인 좌표를 나타내는 N 개의 정수 ai들이 빈칸(공백)을 사이에 두고 주어진다. (1≤ai≤109 )
+두 번째 줄에는 돌들이놓인 좌표를 나타내는 N 개의 정수 ai들이 빈칸(공백)을 사이에 두고 주어진다. (1≤ai≤109 )
 여기서, 주어진 좌표들은 증가하는 순서로 주어지고 모두 다르다.
 세 번째 줄에는 개구리가 한 번의 점프로 이동 가능한 최대 거리 K 가 주어진다. ( 1≤K≤109 )
 
 출력
 각 테스트 케이스의 답을 순서대로 표준출력으로 출력하여야 하며, 각 테스트 케이스마다 첫 줄에 “Case #T”를 출력하여야 한다.
 이때 T는 테스트 케이스의 번호이다.
-그 다음 줄에는 개구리가 마지막 돌로 이동할 수 있는 최소 점프 횟수를 출력한다.
+그 다음 줄에는 개구리가 지막 돌로 이동할 수 있는 최소 점프 횟수를 출력한다.
 만약, 개구리가 마지막 돌로 이동하는 것이 불가능한 경우에는 "-1"을 출력한다.
 
 입출력예
@@ -47,7 +47,7 @@ Case #3
 8
 */
 /*
-You should use the statndard input/output
+You hould use the statndard input/output
 
 in order to receive a score properly.
 
@@ -59,8 +59,10 @@ Please be very careful.
 #include <stdio.h>
 #include <stdbool.h>
 
+void arrInit(int *a);
 int Answer;
-bool isPossibleLanding(int *a, int start, int step);
+int a[1000001] = {0};
+
 
 int main(void)
 {
@@ -92,53 +94,56 @@ int main(void)
 		   The answer to the case will be stored in variable Answer.
 		 */
 		/////////////////////////////////////////////////////////////////////////////////////////////
-		int n, k, a[1000001];
+		int n, k;
 		int now = 0;
 		int count = 0;
-		bool flag = true;
-		a[0] = 1;
+		bool jumpable = true;
+		arrInit(a);
+		
 		scanf("%d", &n);
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++){
 			scanf("%d ", a + i);
 		}
 		scanf("%d", &k);
-		
-		while(flag) {
-			for (int i = k; i > 0; i--) {
-				if (now == a[n]) { 
-					flag = false;
-					Answer = count;
-					break;
-				}
-				if (isPossibleLanding(a, now, i)) {
-					now += i;
+
+		/*debug//////////////////////////////////////////////
+		printf("n = %d, k = %d, *a = [", n, k); 			//
+		for (int i = 0; i <= n; i++){						//
+			printf("%d ", *(a + i));						//
+		}													//
+		printf("\b]\n");									//
+		//debug*//////////////////////////////////////////////
+
+		for (int i = 0; i < n - 1; i++) {
+			if (a[i + 1] - a[i] > k) { //k보다 돌 사이의 거리가 크면 이동할 수 없다.
+				count = -1;
+				jumpable = false;
+			}
+		}
+		while (now != a[n] && jumpable) {
+			for (int i = n; i > 0; i--) {
+				if (a[i] <= now + k) {
+					now = a[i];
+					//printf("now = %d\n", now);
 					count++;
-				} else {
-					flag = false;
-					Answer = -1;
+					i = n;
 					break;
 				}
 			}
 		}
-		
+		Answer = count;
+
 	
         // Print the answer to standard output(screen).
         
 		printf("Case #%d\n", test_case+1);
         printf("%d\n", Answer);
-        
 	}
 
 	return 0;//Your program should return 0 on normal termination.
 }
 
-bool isPossibleLanding(int *a, int start, int step) {
-	for (int i = 0; i < 1000001; i++){
-		if (a[start + i] == 0) { return false; }
-		if (a[start + i] == start + step) { return true; }
-	}
+void arrInit(int *a) {
+	int out[1000001] = {0};
+	a = out;
 }
-
-
-
-
