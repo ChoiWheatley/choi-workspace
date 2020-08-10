e864734186d2095d02af99fffd070c6ea1e9054e
ChoiWheatley <chltmdgus604@naver.com>
2020-08-11 01:58:07 +0900

    117: 일차방정식 <왜 틀렸는지 모름>
    일단 a + b / c 에서 a + b 가 200이 될 수도 있기 때문에 x1_, x2_의 범위를
    200까지로 잡고 다시 돌리니까 문제를 해결했다. 하지만 그래도
    코드그라운드에서는 틀렸다고 나온다.
    
    ㅁ 배운 것들
    1. 함수는 호출시 메모리의 stack 영역에 할당된다.
    2. 반환된 함수는 로컬변수와 함께 메모리에서 해제가 된다.
    3. 로컬변수 중에서 해제가 되면 안 되는 변수는 static 키워드를 사용하면
    함수가 반환돼도, 재호출이 되어도 초기화되지 않고 제 값을 유지한다.
     	- 나는 반환값으로 로컬 int* 형을 넣었더니 warning이 떴다. "너
    이대로 실행하면 리턴하는 배열의 주소도 같이 초기화될걸?" 이라는
    의미였다.
    4. cin.ignore(streamsize) : 스탠다드 인풋의 버퍼를 해당 갯수만큼 버린다.
    5. random 함수 사용법:  #include <cstdlib>, #include <ctime> 불러오고,
    ||||| srand(unsigned int _Seed)의 매개변수로 "static_cast<unsigned
    int>(time(NULL))"을 집어넣은 뒤에, |||||  rand() 함수 호출하면 완료.
    6. c++에서 캐스팅하는 방법으로는 4가지 방법이 있다. 가장 대표적인
    방법으로 static_cast가 있고, const_cast, reinterpret_cast,
    dynamic_cast가 있다. 클래스 상속과 관련한 캐스팅은 나아아아중에
    배우도록.

--- ALGORITHMS/117.cpp
+++ ALGORITHMS/117.cpp
@@ -9,10 +9,79 @@ Please be very careful.
 */
 
 #include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <ctime>
 
 using namespace std;
 
-int Answer;
+int stack_cnt = -1;
+
+void stack_push(char *origin, char w){
+    origin[++stack_cnt] = w;
+    return;
+}
+void stack_flush(char *origin){
+    char tmp;
+    for (int i = 0; tmp != '\0'; i++) {
+        tmp = origin[i];
+        origin[i] = 0;
+    }
+    stack_cnt = -1;
+    return;
+}
+int max(int a, int b) {
+    if (a < b) { return b; }
+    else { return a; }
+}
+//a[] 에서 b[]를 뺍니다 그 반대도 수행합니다.
+void array_mask(int *a_, int *b_) { 
+    int a, b, tmp;
+    for (int i = 2; i <= 200; i++) {
+        a = a_[i];
+        b = b_[i];
+        if (a > 0 && b > 0) {
+            tmp = a - b;
+            b = b - a;
+            a = tmp;
+            if (a < 0) { a = 0; }
+            if (b < 0) { b = 0; }
+        }
+        a_[i] = a;
+        b_[i] = b;
+    }
+}
+
+int multitude(int *arr) {
+    int tmp = 1;
+    for (int i = 2; i <= 200; i++) {
+        if (!arr[i]) { continue; }
+        tmp *= pow(i, arr[i]);
+    }
+    return tmp;
+}
+
+int* short_fraction(int a, int b) {
+    int x1_[201] = {0}, x2_[201] = {0};
+    static int out[2] = {0};
+    for (int i = 2; i <= max(a, b); i++) {
+        if (!(a % i)) { 
+            x1_[i]++; 
+            a /= i--;
+            continue;
+        }
+        if (!(b % i)) { 
+            x2_[i]++; 
+            b /= i--;
+            continue;
+        }
+    }
+    array_mask(x1_, x2_);
+    out[0] = multitude(x1_);
+    out[1] = multitude(x2_);
+
+    return out;
+}
 
 int main(int argc, char** argv)
 {
@@ -27,24 +96,79 @@ int main(int argc, char** argv)
 	   But before submission, you must remove the freopen function or rewrite comment symbols(//).
 	 */	
 
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 
 	cin >> T;
+    cin.ignore(1);
 	for(test_case = 0; test_case  < T; test_case++)
 	{
 
-		Answer = 0;
 		/////////////////////////////////////////////////////////////////////////////////////////////
 		/*
 		   Implement your algorithm here.
 		   The answer to the case will be stored in variable Answer.
 		 */
 		/////////////////////////////////////////////////////////////////////////////////////////////
-		
+        char input_str[17] = {0, };
+        char tmp_stack[6] = {0, };
+        char tmp_char = ' ';
+        int a, b, c; //ax ? b = c;
+        int x1, x2;     //x = x1 / x2
+        
+        cin.getline(input_str, 17);
+
+        for (int i = 0; tmp_char != '\0'; i++) {
+            tmp_char = input_str[i];
+            if (tmp_char == 'x') {
+                a = atoi(tmp_stack);
+                stack_flush(tmp_stack);
+                continue;
+            } else if (tmp_char == '=') {
+                b = atoi(tmp_stack);
+                stack_flush(tmp_stack);
+                continue;
+            } else if (tmp_char == ' ') {
+                continue;
+            } else if (tmp_char == '-') {
+                continue;
+            }
+            stack_push(tmp_stack, tmp_char);
+        }
+        c = atoi(tmp_stack);
+        stack_flush(tmp_stack);
+        //cout << "a = " << a << " b = " << b << " c = " << c << endl;
+
+        x1 = b + c;
+        x2 = a;
+        //cout << x1 << " / " << x2 << "->\t";
+        a = x1;
+        b = x2;
+        x1 = short_fraction(a, b)[0];
+        x2 = short_fraction(a, b)[1];
+        //cout << x1 << " / " << x2 << endl;
+
+
 		// Print the answer to standard output(screen).
 		cout << "Case #" << test_case+1 << endl;
-		cout << Answer << endl;
+		cout << x1 << " / " << x2 << endl;
+
+
 	}
+    /*
+    DEBUG
+    */
+   /*
+    srand(static_cast<unsigned int>(time(NULL)));
+    for (int i = 0; i < 100; i++) {
+        int a = rand();
+        int b = rand();
+        a = (a % 100) + 1;
+        b = (b % 100) + 1;
+        cout << a << " / " << b << "\t -> \t";
+        cout << short_fraction(a, b)[0] << " / " << short_fraction(a, b)[1] << endl;
+    }
+    */
 
 	return 0;//Your program should return 0 on normal termination.
 }
+
