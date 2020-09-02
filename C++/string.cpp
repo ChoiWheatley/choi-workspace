d09f26e098a7fc0d1ba09e7e9d7b5cc142171bee
ChoiWheatley <chltmdgus604@naver.com>
2020-09-02 23:43:45 +0900

    <질문> wcout 스트림에 들어가는 L"뭐시기"가 제대로 출력되지 않는다.
    wcin.imbue( locale("korea") ); 는 한글만을 위한 입출력을 담당하게
    만든다. 참고로 imbue의 뜻은 "느낌이나 생각을 채운다" "to fill something
    or someone with a particular feeling, quality, or idea" 라고 한다.

--- C++/string.cpp
+++ C++/string.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <string>
+#include <locale>
 using namespace std;
 
 int main() {
-    string str1 = "C++ is Cool!";
-    string str2 = str1;
-    string str3 = str2 + " And Clear!";
-    string str4 = "안녕하세요";
+    wcin.imbue(locale("ko-KR"));
+    wcout.imbue(locale("ko-KR"));
+    wstring address, name;
+    wcout << L"자신의 이름을 입력해주세요 :";
+    getline(wcin, name);
+    wcout << L"자신이 살고 있는 도시를 입력해주세요 :";
+    getline(wcin, address);
 
-    cout << str3 + "의 길이는 " + to_string(str3.length()) + " 입니다." << endl;
-    cout << str4 + "의 크기는 " + to_string(str4.size()) + " 입니다." << endl;
-
+    wcout << address << L"에 살고있는 " << name << L"씨 맞으십니까?" << endl;
+    wcout << L"당신의 이름은 총 " << to_wstring(name.size()) << L"글자 이군요! 놀라워요" << endl;
+    for (int i = 0; i < name.size(); i++) {
+        wcout << name[i] << " ";
+    }
+    wcout << endl;
 
 
     return 0;
