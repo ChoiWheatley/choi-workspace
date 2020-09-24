0f531a4dc23297e46f924ede406b1e028a638796
최승현 <choeseunghyeon@choeseunghyeon-ui-MacBookAir.local>
2020-09-24 22:31:21 +0900

    <PPPUC++> Range Error : in vector's case, vector's subscript function (called vector::operator[]) reports finding an error by thorwing an exception names "out_of_range"

--- c++/tmp.cpp
+++ c++/tmp.cpp
@@ -5,40 +5,27 @@
 
 #include "std_lib_facilities.h"
 
-class Bad_area
-{
-public:
-	string name = "bad area";
-}; // a type specifically for reporting errors from area()
-
-int f(int,int,int);
-int framed_area(int,int);
-int area(int, int);
-
-
-
-
 //
 //
 int main()
 {
 	try
 	{
-		int x = 1;
-		int y = 2;
-		int z = 4;
-		int area1 = area(x,y);
-		cout << area1 << endl;
-		int area2 = framed_area(1,z);
-		cout << area2 << endl;
-		int area3 = framed_area(y,z);
-		cout << area3 << endl;
-		double ratio = area1 / area3;
-		cout << ratio << endl;
+		vector<int> v;
+		for (int x; cin >> x; )
+		{
+			v.push_back(x);
+		}
+		for (int i = 0; i <= v.size(); ++i)
+		{
+			cout << "v[" << i << "] == " << v[i] << endl;
+		}
 	}
-	catch(Bad_area e)
+	catch(out_of_range e)
 	{
-		cout << "Ooops! bad arguments to area()\n" << e.name << endl;;
+		cerr << "Ooops! Range Error!!!!\n";
+		cerr << e.what() << endl;
+		return 1;
 	}
 	
 	
@@ -46,31 +33,3 @@ int main()
 }
 //
 //
-
-int f(int x, int y, int z)
-{
-	int area1 = area(x,y);
-	cout << area1 << endl;
-	if (area1 <= 0) error("non-positive area");
-	int area2 = framed_area(1,z);
-	cout << area2 << endl;
-	int area3 = framed_area(y,z);
-	cout << area3 << endl;
-	double ratio = double(area1) / area3;
-	cout << ratio << endl;
-	
-	return area1;
-}
-int framed_area(int x, int y)
-{
-	constexpr int frame_width = 2;
-	return area(x - frame_width, x - frame_width);
-}
-int area(int x, int y)
-{
-	if (x <= 0 || y <= 0)
-	{
-		throw Bad_area{};
-	}
-	return x * y;
-}
