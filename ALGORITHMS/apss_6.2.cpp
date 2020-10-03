2656034fd5d361e087d6aece839bae209fde3df8
ChoiWheatley <chltmdgus604@naver.com>
2020-10-03 18:45:07 +0900

    <APSS> Ch6 code 6.2 중첩 반복문 대체하기기 -- recursive functions
    nCk, n개의 원소 중에서 k개를 뽑는 경우의 수를 조합이라고 부른다.
    자꾸 permutation과 combination을 헷갈리는 것 같은데, 순서를 따지면
    permutation, 순서를 따지지 않고 뽑기만 하면 combination 이다.
    처음에 구현했던 코드는 is_dupl() 함수로 이전에 중복되는 숫자가 있는지만
    확인했다. (abcd) 와 (dcba)는 서로 다른 경우로 판단되었기 때문에
    permutation이 출력된 것이었다.
    수정된 코드에 땨르면 원소들에 부여된 순위를 가지고 is_bigger() 함수로
    한 번 방문했던 원소보다 큰 원소들만 다음 picked 배열에 넣을 수 있게
    만들었다.

--- ALGORITHMS/apss_6.2.cpp
+++ ALGORITHMS/apss_6.2.cpp
@@ -12,6 +12,7 @@ using namespace std;
 void combination(int n, int k, vector<int> & picked);
 void print_combi(const vector<int> & combi);
 bool is_dupl(int i, const vector<int> & combi);
+inline bool is_bigger(int i, const vector<int> & combi);
 
 /*
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
@@ -21,8 +22,8 @@ bool is_dupl(int i, const vector<int> & combi);
 int main(void)
 {
     int n = 0, k = 0;
-    cout << "Please input 'n' and 'k' which infers 'choose k elements from the sequence 0 to n'\n$ ";
-    cin >> n >> k;
+    cout << "Please input 'n' and 'k' which infers 'n Combination of k'\n>>> ";
+    cin >> k >> n;
     combination(n, k, *(new vector<int>));
     
     return 0;
@@ -35,14 +36,32 @@ int main(void)
 
 void combination(int n, int k, vector<int> & picked)
 /*
+ * pre-condition : n < 4인 경우, 오류를 내고 프로그램을 종료한다.
  * base case : k == 0인 경우에 picked를 출력한다.
+ * base case : picked.empty()인 경우에 0을 집어넣는다.
  * 각 함수는 원소를 하나씩만 picked에 넣고 나머지 k-1개의 조합은 재귀함수로 찾는다.
  */ 
 {
-    if ( k == 0 ) print_combi(picked);
+    static int count = 0;
+    if ( n < k ) throw runtime_error("'n' should be bigger than k");
+    if ( k == 0 ) 
+    {
+        print_combi(picked);
+        count++;
+        return;
+    }
     for (int i = 0; i < n; i++)
     {
+        /* permutation 을 구하는 코드였다.
         if (!is_dupl(i, picked))
+        {
+            picked.push_back(i);
+            combination(n, k-1, picked);
+            picked.pop_back();
+        }
+        */
+        // combination
+        if (is_bigger(i, picked))
         {
             picked.push_back(i);
             combination(n, k-1, picked);
@@ -69,4 +88,14 @@ bool is_dupl(int i, const vector<int> & combi)
         if (i == j) return true;
     }
     return false;
+}
+
+inline bool is_bigger(int i, const vector<int> & combi)
+{
+    if (combi.empty())
+        return true;
+    if (i > combi.back())
+        return true;
+    else 
+        return false;
 }
