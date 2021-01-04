aacb1709207114d9670047439b6cc2ec103bf51f
ChoiWheatley <chltmdgus604@naver.com>
2021-01-04 18:44:09 +0900

    <PPPUC++> Chapter10 Drill5 <Complete>
    when you want to get all whitespace, use istream::get()
    got confused with skipping to the next line!!!

--- c++/point.cpp
+++ c++/point.cpp
@@ -1,5 +1,5 @@
 #include "std_lib_facilities.h"
-using namespace std;
+#include <unistd.h>
 
 struct Point {
     int x;
@@ -15,29 +15,73 @@ Point prompt_point() {
 
 ostream& operator<<(ostream& os, Point& p);
 ostream& operator<<(ostream& os, vector<Point>& p);
+istream& operator>>(istream& is, Point& p);
+istream& operator>>(istream& is, vector<Point>& p);
+
 
 
 int main(void) 
 {
     vector<Point> original_points;
+    vector<Point> processed_points;
     for (int i = 0; i < 7; i++) {
         original_points.push_back(prompt_point());
     }
     cout << original_points;
 
-    ofstream os{"mydata.txt"};
+    // Drill4. Open an ofstream and output each point to a file
+    ofstream os;
+    os.open("mydata.txt");
     if (!os) error("cannot open file. please check if the file names isn't correct");
     os << original_points;
-    
+    os.close();
+
+    // Drill5. Close the ofstream and then open an ifstream for "mydata.txt"
+    ifstream is;
+    is.open("mydata.txt");
+    if (!is) error("cannot open file. please check if the file names isn't correct");
+    // get points from is
+    is >> processed_points;
+    cout << processed_points;
+
 
     return 0;
 }
 
 ostream& operator<<(ostream& os, Point& p) {
-    os << "[" << p.x << ", " << p.y << "]\n";
+    os << p.x << " " << p.y << "\n";
     return os;
 }
 ostream& operator<<(ostream& os, vector<Point>& p) {
     for (auto i : p) os << i;
     return os;
+}
+istream& operator>>(istream& is, Point& p) {
+    int x, y;
+    is >> x >> y;
+    p = Point{x, y};
+    return is;
+}
+// 동작안함 
+istream& operator>>(istream& is, vector<Point>& p) {
+    is.exceptions(is.exceptions() | ios_base::badbit);
+    for (Point pnt; true; ) {
+        is >> pnt;
+        //cout << (is.good() ? "good, " : is.eof() ? "eof, " : is.fail() ? "fail " : "bad ");
+        if (is.good())
+            p.push_back(pnt);
+        if (is.eof()) 
+            return is;
+        if (is.fail()) { // format error
+            is.clear();
+            char ch;
+            for (ch = is.get(); (is && ch != '\n') ; ch = is.get()) {
+                /* do nothing */
+            }
+            if (!is) error("somthing wrong with istream");
+            is.unget();
+        }
+    }
+    
+    return is;
 }
