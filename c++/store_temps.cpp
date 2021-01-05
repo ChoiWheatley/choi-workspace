dc0f32bb4ef9c856e827e000f07bd81307978503
ChoiWheatley <chltmdgus604@naver.com>
2021-01-05 18:26:50 +0900

    <PPPUC++> Chapter10 Exercise2 <Complete>
    random_double() function confusing me

---
+++ c++/store_temps.cpp
@@ -0,0 +1,26 @@
+#include "std_lib_facilities.h"
+
+
+struct Reading {
+    int hour;
+    double temperature;
+};
+
+double random_double(int dec, double min, double max);
+
+int main() {
+    ofstream of{"raw_temps.txt"};
+    if (!of) error("something wrong opening the file");
+    srand(time(NULL));
+    for (int i = 0; i < 50; i++) {
+        int hr = (int)random_double(0, 0, 23);
+        double temp = random_double(3, -30, 100);
+        of << hr << '\t' << temp << '\n';
+    }
+    return 0;
+}
+double random_double(int dec, double min, double max) {
+    return (
+        (double)((long int)rand() %
+                 (long int)((max - min + 1) * pow(10,dec)) / pow(10,dec) + min));
+}
