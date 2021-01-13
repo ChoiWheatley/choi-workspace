e09638413ba751a806db979487982b98c08556d8
ChoiWheatley <chltmdgus604@naver.com>
2021-01-13 16:03:02 +0900

    <PPPUC++> Chapter11 Exercise6 <On Progress>
    Second attempt : find out the bool() operator was the trap.
    but while revising, I againsted a problem, when src_ meets eof bit,
    the program exits the loop without printing any other remaining
    last line.

--- c++/punc.cpp
+++ c++/punc.cpp
@@ -5,7 +5,13 @@
  * Don’t modify characters within a pair of double quotes ("). 
  * For example, “ - don't use the as-if rule.” becomes “ don t use the as if rule ”.
  * 
+ * << Second attempt 2021. 01. 13 >>
+ * - Problem : The last line cannot printed at all. I think src_ met eof() and the next loop, 
+ *      because the exit condition is satisfacted, the program exit the loop.
+ * 
  * << First attempt 2021. 01. 12 >>
+ * - something has wrong with this program. Puncstream vomitted during eating process and
+ *      straightly met end of loop.
  * 
  * <<Thinking>>
  * <> Can I eliminate ' ' as whitespace? And is it worth it?
@@ -16,67 +22,78 @@
 
 #include "std_lib_facilities.h"
 
-namespace PUNC {
-
-    class Puncstream {
-    public:
-        Puncstream(istream& is) : source{is} {}
-        Puncstream(istream& is, string punc, bool sensitive = false) : 
-            source{is}, punctuation{punc}, case_sensitive{sensitive} {};
-        inline bool is_punc(char c);
-        operator bool();
-        Puncstream& operator>>(string&);
-    private:
-        string punctuation = ".;,?-'";
-        istream& source;
-        istringstream buffer;
-        bool case_sensitive;
+namespace Punc
+{
+    enum class P_ERR {
+        good,
+        eof,
+        fail,
+        bad
     };
-
-}
-
-
+    class Puncstream
+    {
+    public:
+        Puncstream(istream &is, bool case_sensitive = false, string punctuation = ".,;!?|~'-") : src_{is}, punctuation_{punctuation}, case_sensitive_{case_sensitive} {}
+        bool is_case_sensitive() const { return case_sensitive_; }
+        operator bool() {return !(src_.eof() || src_.bad() || buffer_.bad());}
+        bool is_punctuation(char) const;
+        istringstream& get_buffer() {return buffer_;}
+        // from src_, we convert defined whitespace into buffer each line,
+        // and we do buffer >> str until buffer is nothing left.
+        Puncstream& operator>> (string& str);
+    private:
+        istream &src_;
+        istringstream buffer_;
+        bool case_sensitive_;
+        string punctuation_;
+    };  // class Puncstream
+    void show_buffer(istringstream& buffer);
+} // namespace Punc
 int main() 
 {
     ifstream ifs{"sample.txt"};
     ofstream ofs{"output.txt"};
-    if(!ifs) error("error while opening this file");
-    PUNC::Puncstream ps{ifs, ".;?,-'"};
-    for (string str; ps >> str; ) {
-        static int cnt = 1;
-        if ((cnt++ %10) == 0) ofs << '\n';
+    if (!ifs || !ofs) error("error opening the file");
+    Punc::Puncstream ps{ifs, true};
+    for (string str; ps >> str; )
+    {
+        static int cnt = 0;
         ofs << str << '\t';
+        if ((++cnt %20) == 0) ofs << '\n';
     }
     return 0;
 }
 
 
-
-namespace PUNC{
-    inline bool Puncstream::is_punc(char c) {
-        for (char i : Puncstream::punctuation) if (c == i) return true;
+namespace Punc 
+{
+    bool Puncstream::is_punctuation(char c) const {
+        for (auto i : punctuation_) 
+            if (c == i) return true;
         return false;
     }
-    Puncstream& Puncstream::operator>>(string & str) {
-        while(!(buffer >> str)) {
-            if (buffer.bad() || !source.good()) return *this;
-            buffer.clear();
-            // replenish buffer 
+    // from src_, we convert defined whitespace into buffer each line,
+    // and we do buffer >> str until buffer is nothing left.
+    Puncstream &Puncstream::operator>>(string& str) {
+        while (!(buffer_ >> str)) {
+            if (buffer_.bad() || !src_.good()) return *this;
+            buffer_.clear();
             string line;
-            getline(source, line);
-
-            // do character replacement as needed
-            for (char& ch : line) {
-                if (is_punc(ch)) ch = ' ';
-                else if (case_sensitive)
-                    ch = tolower(ch);
+            getline(src_, line);
+            // punctuation into whitespace 
+            for (char& c : line) {
+                if (is_punctuation(c)) c = ' ';
+                if (is_case_sensitive()) c = tolower(c);
             }
-            buffer.str(line);       // put string into stream
+            buffer_.str(line);
         }
         return *this;
     }
-    Puncstream::operator bool() {
-        return !(source.fail() || source.bad()) && buffer.good();
+    void show_buffer(istringstream& buffer) {
+        for (string str; buffer >> str; ) {
+            static int cnt = 0;
+            cout << str << '\t';
+            if ((++cnt %20) == 0) cout << "\n";
+        }
     }
 }
-
