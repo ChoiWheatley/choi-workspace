4d0bc05408155540e56e96ed1cab93d7ccbe0668
ChoiWheatley <chltmdgus604@naver.com>
2021-01-19 18:24:06 +0900

    <FLTK> Drawing Things in FLTK <<On Progress>>
    <Done>
    - Drawing Boxes
    - Clippiing (I couldn't understand)
    - Colors
    
    <Left>
    - Line Dashes and Thickness
    - Drawing Fast Shapes
    - Drawing Complex Shapes
    - Drawing Text
    - Fonts
    - Character Encoding
    - Drawing overlays
    - Drawing Images
    
    <TODO>
    <> Makefile configuration
    <> how to change fltk-config's configuration such as compile options?

---
+++ c++/fltk-drawing.cpp.app/Contents/MacOS/fltk-drawing.cpp
@@ -0,0 +1,62 @@
+/*
+ * << FLTK >> Drawing things in FLTK
+ */
+
+#include <FL/Fl.H>
+#include <FL/fl_draw.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Enumerations.H>
+
+class NewDraw : public Fl_Widget {
+    public:
+        NewDraw(int x, int y, int w, int h, const char *label = (const char *)0);
+        ~NewDraw();
+        void draw() override;
+};
+
+int main(int argc, const char ** argv) {
+    Fl_Window win(1000, 800, "Draw everything");
+    NewDraw d{0, 0, 1000, 800};
+    d.draw();
+    win.show();
+    return Fl::run();
+}
+
+NewDraw::NewDraw(int x, int y, int w, int h, const char *label) :
+    Fl_Widget(x, y, w, h, label)
+{}
+NewDraw::~NewDraw() {  }
+void NewDraw::draw() {
+    // add clip
+    // you can limit all your drawing to a rectangular region.
+    // if you draw anything out of this region,,, Segmentation fault 
+    // will occur. 왜만든거야
+    //fl_push_clip(1, 1, 100, 200);
+    // draw box
+    fl_draw_box(Fl_Boxtype::FL_UP_BOX, 1, 1, 99, 99, FL_RED);
+    // draw frame
+    fl_frame("XXAA",1, 101, 99, 99);
+    // pop clip back
+    // so you can draw outside
+    //fl_pop_clip();
+    fl_draw_box(FL_UP_BOX, 1, 201, 99, 99, fl_rgb_color(0xab));
+
+    //colorpaletes 
+    constexpr int startx = 101;
+    constexpr int starty = 1;
+    constexpr int side = 10;
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 8; j++) {
+            static Fl_Color c =  fl_color_cube(0, 0, 0);
+            fl_draw_box(Fl_Boxtype::FL_THIN_UP_BOX,
+                        startx + (j * side),
+                        starty + (i * side), 
+                        side,
+                        side,
+                        fl_rgb_color(c++)
+            );
+        }
+    }
+}
