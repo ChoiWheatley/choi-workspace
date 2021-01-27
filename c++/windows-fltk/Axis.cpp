1b784462f85d2521027dd1947b7cbaf9fa1978d9
ChoiWheatley <chltmdgus604@naver.com>
2021-01-27 15:29:04 +0900

    <PPPUC++> 12.7.3 Axis Manually Implemented <Completed>
    <Done>
    - Same Interface with PPP2 version of instantiating Axis Class (x,y,length,notches,orientation,label)
    - Own custom class which derived from Fl_Widget class
    - setcolor(Fl_Color) method
    - It was quite messy putting notches inside the axis....
    
    <Tip>
    - When you decided to draw your own Widget, every drawing functionalities are kept inside the draw() function.
    
    <????>
    - I couldn't make draw() funtions transform or translate into some position, rotation, etc... Why?

---
+++ c++/windows-fltk/Axis.cpp
@@ -0,0 +1,50 @@
+﻿#include "Axis.h"
+
+Axis::Axis(int x, int y, int length, int notches, orientation o, const char* label) :
+	Fl_Widget{ x, y, length, length, label }, length_{ length }, notches_{ notches }, o_{ o }
+{}
+
+void Axis::setcolor(Fl_Color bg)
+{
+	color_ = bg;
+	redraw();
+}
+
+void Axis::draw() {
+	// first draw vertically, next rotate whether orientation is x
+	int x = this->x();
+	int y = this->y();
+	fl_color(color_);
+	switch (o_) {
+	case orientation::x:
+	{
+		fl_line(x, y, x, y + width_);
+		fl_line(x, y + width_, x + length_, y + width_);
+		fl_line(x + length_, y + width_, x + length_, y);
+		for (int i = 0; i < notches_; i++) {
+			static int sum = 0; 
+			static int len = length_;
+			sum += static_cast<int>(len / (notches_ - i));
+			len = length_ - sum;
+			fl_line(x + sum, y + width_, x + sum, y);
+		}
+		fl_draw(label(), x + static_cast<int>(length_ / 2), y + width_ * 2);
+		break;
+	}
+	case orientation::y:
+	{
+		fl_line(x, y, x + width_, y);
+		fl_line(x, y, x, y + length_);
+		fl_line(x, y + length_, x + width_, y + length_);
+		for (int i = 0; i < notches_; i++) {
+			static int sum = 0;
+			static int len = length_;
+			sum += static_cast<int>(len / (notches_ - i));
+			len = length_ - sum;
+			fl_line(x, y + sum, x + width_, y + sum);
+		}
+		fl_draw(label(), x - sizeof(label()), y - fl_height());
+		break;
+	}
+	}
+}
--- c++/windows-fltk/fl-drawing.cpp
+++ c++/windows-fltk/fl-drawing.cpp
@@ -1,36 +1,53 @@
-/*
+﻿/*
  * <FLTK> Drawing Things in FLTK
  *
  * < Author > ChoiWheatley
  * < Doc Link > https://www.fltk.org/doc-1.3/drawing.html
  *
  * < ToDo >
- * <> Mac Visual Studio ��ġ�Ͽ� �ش� �������丮 ����ͼ� fltk ���� �������� Ȯ���ϱ�
+ * <> Mac Visual Studio 설치하여 해당 리포지토리 끌어와서 fltk 빌드 가능한지 확인하기
  *
  *
  * < History >
+ * 2021. 01. 26.
+ *  -- PPPUC++ 본격 진도빼기 without custom header
  * 2021. 01. 21.
- *	-- �����쿡�� fltk �絵�� �ϱ�. Visual Studio���� ����� ���ư���.
- *	-- ��ǥ : [ Drawing Fast Shapes ] & [ Drawing Complex Shapes ] & [ Drawing Text ] & [ Fonts ]
+ *	-- 윈도우에서 fltk 재도전 하기. Visual Studio에서 제대로 돌아간다.
+ *	-- 목표 : [ Drawing Fast Shapes ] & [ Drawing Complex Shapes ] & [ Drawing Text ] & [ Fonts ]
  * 
  * < Compile Options via Visual Studio >
  * reference from : https://alf-p-steinbach.github.io/Install-FLTK-for-use-with-Visual-C-/
  * 
  */
 
+#ifndef AXIS_H
+#define AXIS_H
 #include <FL/Fl.H>
+#include <FL/Fl_Widget.H>
 #include <FL/fl_draw.H>
+#include <iostream>
+#include <vector>
+#endif
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Text_Editor.H>
 #include <FL/Fl_Window.H>
+#include "Axis.h"
 
 class MyWindow : public Fl_Window {
 public:
 	MyWindow(int w, int h, const char* label = 0) : Fl_Window{ w, h, label } {}
 	~MyWindow() {}
-	virtual void draw() override;
+	virtual void draw();
 private:
+};
+
+class MyWidget : public Fl_Widget {
+public:
+	MyWidget(int x, int y, int w, int h, const char* label = 0) : Fl_Widget(x, y, w, h, label) {}
+	~MyWidget() {}
+private:
+	virtual void draw() override;
 };
 
 struct Point {
@@ -53,22 +70,52 @@ Point gaps[] = {
 	{165, 320},
 };
 
+/*
+███    ███  █████  ██ ███    ██ 
+████  ████ ██   ██ ██ ████   ██ 
+██ ████ ██ ███████ ██ ██ ██  ██ 
+██  ██  ██ ██   ██ ██ ██  ██ ██ 
+██      ██ ██   ██ ██ ██   ████ 
+*/
+MyWidget* wid = new MyWidget(0, 0, 640, 480);
 int main(int argc, char** argv)
 {
-	MyWindow* win = new MyWindow(640, 480, "My Window");
-	// ToDo : Write down your own code below
-	win->show();
+	MyWindow*	win = new MyWindow(1080, 720, "냠냠 옴뇸뇸?");
+	MyWidget*	wid = new MyWidget(10, 10, win->w(), win->h());
+	Axis* xa = new Axis(100, 600, 500, 25, Axis::orientation::x, "x axis");
+	Axis* ya = new Axis(100, 100, 500, 32, Axis::orientation::y, "y axis");
+	xa->setcolor(FL_DARK_RED);
+	ya->setcolor(FL_DARK_CYAN);
+	win->show(argc, argv);
 	return Fl::run();
 }
 
 void MyWindow::draw()
 {
 	Fl_Window::draw();
-	// Draw Complex Shapes
-
-	
+	// Draw Text
+	fl_font(FL_HELVETICA, 50);
+	fl_draw("MyWindow!", 300, 100);
+	fl_translate(100, 300);
+	std::cout << "fl_height() = " << fl_height() << '\n';
 }
 
-
-
-
+void MyWidget::draw()
+{
+	// Draw something
+	fl_draw_box(FL_UP_BOX, 10, 10, 100, 100, FL_BLUE);
+	// draw red triangle by three points
+	const static std::vector<Point> vertices{
+		{300, 200},
+		{350, 100},
+		{400, 200}
+	};
+	fl_color(FL_RED);
+	fl_translate(500, 0);
+	fl_begin_loop();
+	for (const Point& i : points) {
+		fl_vertex(i.x, i.y);
+	}
+	fl_end_loop();
+	fl_color(0);
+}
