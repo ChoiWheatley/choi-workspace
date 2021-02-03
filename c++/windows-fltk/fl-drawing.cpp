d82134127e1aac18f635981a89f0e9bb330250cc
ChoiWheatley <chltmdgus604@naver.com>
2021-02-03 20:46:14 +0900

    <PPPUC++> Ch12 Ex5 <Complete>
    I was trying to use FLTK-Event Handling Functions, but it is quite hard to implement protected functions. {clear_visible(), damage(), draw_box(), draw_focus(), draw_labe(), set_flag(), set_visible(), test_shortcut(), type()}
    So I just move window object into global function, using my own widget class to draw rectangle. when resizing window, window object calls its draw() method and inside the window object's draw() method either.

--- c++/windows-fltk/fl-drawing.cpp
+++ c++/windows-fltk/fl-drawing.cpp
@@ -10,7 +10,7 @@
  *
  * < History >
  * 2021. 02. 03
- *  -- <PPPUC++> Chapter 6 Exercises{1, 2, 4}
+ *  -- <PPPUC++> Chapter 6 Exercises{1, 2, 4, 5}
  * 2021. 01. 30.
  *  -- Drawing Images & Circle & Ellipse & Resizable & Mask  
  * 2021. 01. 28.
@@ -80,24 +80,10 @@ struct Point {
 ██  ██  ██ ██   ██ ██ ██  ██ ██ 
 ██      ██ ██   ██ ██ ██   ████ 
 */
+MyWindow  *	win 		= new MyWindow(1080, 720, "나의 창문");
 int main(int argc, char** argv)
 {
-// void Fl_Window::size_range(int minw, int minh, int maxw=0, int maxh=0, int dw=0, int dh=0, int aspect=0)
-	MyWindow  *	win 		= new MyWindow(1080, 720, "나의 창문");
-	Rectangle tic_tac_toe[9]{
-		{10, 10, 10, 10, 0, 0, FL_WHITE},
-		{21, 10, 10, 10, 0, 0, FL_RED},
-		{32, 10, 10, 10, 0, 0, FL_WHITE},
-		{10, 21, 10, 10, 0, 0, FL_RED},
-		{21, 21, 10, 10, 0, 0, FL_WHITE},
-		{32, 21, 10, 10, 0, 0, FL_RED},
-		{10, 32, 10, 10, 0, 0, FL_WHITE},
-		{21, 32, 10, 10, 0, 0, FL_RED},
-		{32, 32, 10, 10, 0, 0, FL_WHITE}
-	};
-	for (int i = 0; i < 9; i++)
-		win->add(tic_tac_toe[i]);
-
+	MyWidget * mw = new MyWidget(0, 0, win->w(), win->h());
 
 	win->size_range(250, 100, 0, 0);
 	win->show();
@@ -107,9 +93,25 @@ int main(int argc, char** argv)
 void MyWindow::draw()
 {
 	Fl_Window::draw();
+	std::stringstream strs;
+	strs << "X = " << this->x() << ", Y = " << this->y() << ", W = " << this->w() << ", H = " << this->h();
+	int alignX = static_cast<int>(this->w()/2 - (fl_width(strs.str().c_str(), strs.str().size())/2));
+	int alignY = static_cast<int>(10 + fl_height() - fl_descent());
+	fl_color(0);
+	fl_draw(strs.str().c_str(), alignX, alignY);
+	fl_color(0);
 }
 
 void MyWidget::draw()
 {
+	int recX = static_cast<int>(win->w() / 6);
+	int recY = static_cast<int>(win->h() / 8);
+	int recW = static_cast<int>(win->w() / 3 * 2);
+	int recH = static_cast<int>(win->h() / 4 * 3);
+	fl_color(FL_RED);
+	fl_line_style(FL_SOLID, 4);
+	fl_rect(recX, recY, recW, recH);
+	fl_color(0);
+	fl_line_style(0);
 }
 
