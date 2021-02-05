47e2913b5e05dd3f40b81d7ad7fcbf811606a892
ChoiWheatley <chltmdgus604@naver.com>
2021-02-05 16:11:42 +0900

    <PPPUC++> Ch12 Ex6 <Complete>
    - When you draw something that is too big than its window, window just draw as possible as its boundary. The shape will be drawn only the fragment of its window
    - When you draw window too big that doesen't match into screen size, it automatically shrink to its screensize.

--- c++/windows-fltk/fl-drawing.cpp
+++ c++/windows-fltk/fl-drawing.cpp
@@ -9,8 +9,10 @@
  *
  *
  * < History >
+ * 2021. 02. 05
+ *	-- Chapter126 Exercise {6, 7, 8, 9, 10???!!!, 11, 12}
  * 2021. 02. 03
- *  -- <PPPUC++> Chapter 6 Exercises{1, 2, 4, 5}
+ *  -- <PPPUC++> Chapter12  Exercises{1, 2, 4, 5}
  * 2021. 01. 30.
  *  -- Drawing Images & Circle & Ellipse & Resizable & Mask  
  * 2021. 01. 28.
@@ -80,10 +82,14 @@ struct Point {
 ██  ██  ██ ██   ██ ██ ██  ██ ██ 
 ██      ██ ██   ██ ██ ██   ████ 
 */
-MyWindow  *	win 		= new MyWindow(1080, 720, "나의 창문");
 int main(int argc, char** argv)
 {
-	MyWidget * mw = new MyWidget(0, 0, win->w(), win->h());
+	// draw a window that is too big to match its screen (2560 * 1600)
+	MyWindow  *	win 		= new MyWindow(5000, 3500, "Ch12 Exercise6: fittness inside the window/screen test");
+	// draw a shape too much bigger than the boundary of its window
+	Rectangle* rec = new Rectangle{20, 20, 5000, 3500};
+	rec->line_style(FL_CYAN, 0, 5);
+	rec->fill_color(FL_GREEN);
 
 	win->size_range(250, 100, 0, 0);
 	win->show();
@@ -95,23 +101,9 @@ void MyWindow::draw()
 	Fl_Window::draw();
 	std::stringstream strs;
 	strs << "X = " << this->x() << ", Y = " << this->y() << ", W = " << this->w() << ", H = " << this->h();
-	int alignX = static_cast<int>(this->w()/2 - (fl_width(strs.str().c_str(), strs.str().size())/2));
-	int alignY = static_cast<int>(10 + fl_height() - fl_descent());
-	fl_color(0);
-	fl_draw(strs.str().c_str(), alignX, alignY);
-	fl_color(0);
 }
 
 void MyWidget::draw()
 {
-	int recX = static_cast<int>(win->w() / 6);
-	int recY = static_cast<int>(win->h() / 8);
-	int recW = static_cast<int>(win->w() / 3 * 2);
-	int recH = static_cast<int>(win->h() / 4 * 3);
-	fl_color(FL_RED);
-	fl_line_style(FL_SOLID, 4);
-	fl_rect(recX, recY, recW, recH);
-	fl_color(0);
-	fl_line_style(0);
 }
 
