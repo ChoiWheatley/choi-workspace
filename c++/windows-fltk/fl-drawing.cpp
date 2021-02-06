668bef16fd79b13b0b98566a71ce5fcde88f6ae2
ChoiWheatley <chltmdgus604@naver.com>
2021-02-06 16:17:32 +0900

    <PPPUC++> Ch12 Exercise7 <Stuck>
    main 함수에 fl_*_polygon() 그릴 수 있도록 Wrapper class 를 만들어야 한다.

--- c++/windows-fltk/fl-drawing.cpp
+++ c++/windows-fltk/fl-drawing.cpp
@@ -84,23 +84,67 @@ struct Point {
 */
 int main(int argc, char** argv)
 {
-	// draw a window that is too big to match its screen (2560 * 1600)
-	MyWindow  *	win 		= new MyWindow(5000, 3500, "Ch12 Exercise6: fittness inside the window/screen test");
-	// draw a shape too much bigger than the boundary of its window
-	Rectangle* rec = new Rectangle{20, 20, 5000, 3500};
-	rec->line_style(FL_CYAN, 0, 5);
-	rec->fill_color(FL_GREEN);
+	MyWindow  *	win 		= new MyWindow(1000, 1000, "Ch12 Exercise7: Draw a 2-D House");
 
 	win->size_range(250, 100, 0, 0);
 	win->show();
 	return Fl::run();
 }
 
+using namespace std;
+vector<Point> roof{
+	{450, 0},
+	{300, 200},
+	{600, 200},
+	{450, 0}
+};
+vector<Point> door_dividing{
+	{450, 400-115+5},
+	{450, 400-5}
+};
+vector<Point> land{
+	{50, 400},
+	{950, 400}
+};
+vector<Point> burrow{
+	{300, 400},
+	{200, 600},
+	{300, 600},
+	{230, 900},
+	{400, 900},
+	{450, 800},
+	{650, 900},
+	{750, 900},
+	{600, 600},
+	{750, 400},
+	{300, 400}
+};
+Rectangle goggle{300,245,300,50};
+Rectangle door{450-23,400-115, 45, 115};
+Circle door_handle1{450-12, 350, 5};
+Circle door_handle2{450+12, 350, 5};
+Rectangle ladder_bound{310, 400, 50, 500};
+Rectangle trunk{700, 200, 30, 200};
+Circle tree_leaves1{700,230,25};
+Circle tree_leaves2{725,210,40};
+Circle tree_leaves3{720,200,30};
+Circle tree_leaves4{715,195,10};
+
 void MyWindow::draw()
 {
 	Fl_Window::draw();
-	std::stringstream strs;
-	strs << "X = " << this->x() << ", Y = " << this->y() << ", W = " << this->w() << ", H = " << this->h();
+// roof
+	fl_color(0x45737300);
+	fl_begin_polygon();
+	for (auto i : roof)
+		fl_vertex(i.x, i.y);
+	fl_end_polygon();
+	fl_color(0);
+// building
+	Rectangle building{300,200,300,200};
+	building.fill_color(0x5a686800);
+	building.line_style(0x10252600, 0, 3);
+	this->add(building);
 }
 
 void MyWidget::draw()
