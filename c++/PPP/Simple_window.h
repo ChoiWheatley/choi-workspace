f3027af4e9b0823896bf991b2e4225da4cdaf95d
ChoiWheatley <chltmdgus604@naver.com>
2021-02-10 22:01:43 +0900

    <PPPUC++> ChoiWheatly Never Giveup <DoItAgain>
    <Done>
    - Window class implemented
    - Point struct implemented
    - Makefile : from compile to archive, linking success
    <TODO>
    - Implement those classes {Shape class, many child classes derived from Shape class, Widget class}
    
    <Point>
    - Don't too be obsessed with structures from PPP's header

--- c++/PPP/Simple_window.h
+++ c++/PPP/Simple_window.h
@@ -12,15 +12,16 @@ struct Simple_window : Window {
 	  button_pushed(false),
 	  next_button(Point(x_max()-70,0), 70, 20, "Next", cb_next) { attach(next_button); }
 	
-	bool wait_for_button();
+	bool wait_for_button()
 	// modified event loop
 	// handle all events (as per default), but quit when button_pushed becomes true
 	// this allows graphics without control inversion
-	//{
-//		while (!button_pushed) Fl::wait();
-//		button_pushed = false;
-//		Fl::redraw();
-//	}
+	{
+		while (!button_pushed) Fl::wait();
+		button_pushed = false;
+		Fl::redraw();
+		return button_pushed;
+	}
 
 	Button next_button;
 private:
