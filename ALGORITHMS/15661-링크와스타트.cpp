80d0526540c145bd3641781948839eea126847f3
ChoiWheatley <chltmdgus604@naver.com>
2021-03-05 07:34:18 +0000

    <BAK> Ch2 15661-링크와스타트 <pre-Submit>
    ssh 에서 접속한 vim 에서는 파일을 바로 클라이언트 클립보드로 옮겨넣지를 못한다. 매우 불편한 점.

--- ALGORITHMS/15661-링크와스타트.cpp
+++ ALGORITHMS/15661-링크와스타트.cpp
@@ -1,6 +1,6 @@
 /*
 문제
-오늘은 스타트링크에 다니는 사람들이 모여서 축구를 해보려고 한다. 축구는 평일 오후에 하고 의무 참석도 아니다. 축구를 하기 위해 모인 사람은 총 N명이고 신기하게도 N은 짝수이다. 이제 N/2명으로 이루어진 스타트 팀과 링크 팀으로 사람들을 나눠야 한다.
+오늘은 스타트링크에 다니는 사람들이 모여서 축구를 해보려고 한다. 축구는 평일 오후에 하고 의무 참석도 아니다. <<<< 두 팀의 인원수는 같지 않아도 되지만, 한 명 이상이어야 한다. >>>>
 
 BOJ를 운영하는 회사 답게 사람에게 번호를 1부터 N까지로 배정했고, 아래와 같은 능력치를 조사했다. 능력치 Sij는 i번 사람과 j번 사람이 같은 팀에 속했을 때, 팀에 더해지는 능력치이다. 팀의 능력치는 팀에 속한 모든 쌍의 능력치 Sij의 합이다. Sij는 Sji와 다를 수도 있으며, i번 사람과 j번 사람이 같은 팀에 속했을 때, 팀에 더해지는 능력치는 Sij와 Sji이다.
 
@@ -113,7 +113,6 @@ int team_balance(const vector<vector<int>>& player, int idx, vector<int> team_st
 {
 	//BASECONDITION
 	if (idx == N) {
-		if (team_start.size() != team_link.size()) return INT_MAX;		
 #if DBG > 1
 		cout << "team_start = \n" << team_start;
 		cout << "team_link = \n" << team_link;
@@ -122,9 +121,6 @@ int team_balance(const vector<vector<int>>& player, int idx, vector<int> team_st
 #endif
 		return abs(synergy(player, team_start) - synergy(player, team_link));
 	}
-	//UNNECESSARY CONDITION : Backtraking Method
-	if (team_start.size() > N/2 || 
-		team_link.size() > N/2) return INT_MAX;
 
 	int min_gap = INT_MAX;
 	// idx -> team_start
