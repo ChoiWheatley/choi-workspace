ae5f57291c79bb78df0228d7115bba116b2643da
ChoiWheatley <chltmdgus604@naver.com>
2021-03-08 12:08:12 +0000

    <BAK> Ch3-4 10819-차이를최대로 <complete>
    Tip: next permutation algorithm : nonincreasing order / prev permutation : nondecreasing order
    같은 수가 있다는 점을 감안했을 때 j 인덱스 값을 찾는 상황에서 중복을 허용해줘야 한다.
    1. A[i-1] == A[i] 일 때 중복을 허용하고 i 를 정해버리면 무한루프에 빠진다.

--- ALGORITHMS/10973-이전순열.cpp
+++ ALGORITHMS/10973-이전순열.cpp
@@ -1,6 +1,6 @@
 /*
 문제
-1부터 N까지의 수로 이루어진 순열이 있다. 이때, 사전순으로 다음에 오는 순열을 구하는 프로그램을 작성하시오.
+1부터 N까지의 수로 이루어진 순열이 있다. 이때, 사전순으로 바로 이전에 오는 순열을 구하는 프로그램을 작성하시오.
 
 사전 순으로 가장 앞서는 순열은 오름차순으로 이루어진 순열이고, 가장 마지막에 오는 순열은 내림차순으로 이루어진 순열이다.
 
@@ -16,24 +16,24 @@ N = 3인 경우에 사전순으로 순열을 나열하면 다음과 같다.
 첫째 줄에 N(1 ≤ N ≤ 10,000)이 주어진다. 둘째 줄에 순열이 주어진다.
 
 출력
-첫째 줄에 입력으로 주어진 순열의 다음에 오는 순열을 출력한다. 만약, 사전순으로 마지막에 오는 순열인 경우에는 -1을 출력한다.
+첫째 줄에 입력으로 주어진 순열의 이전에 오는 순열을 출력한다. 만약, 사전순으로 가장 처음에 오는 순열인 경우에는 -1을 출력한다.
 
 예제 입력 1  복사
 4
 1 2 3 4
 예제 출력 1  복사
-1 2 4 3
+-1
 예제 입력 2  복사
 5
 5 4 3 2 1
 예제 출력 2  복사
--1
+5 4 3 1 2
 */
 #include<iostream>
 #include<vector>
 
-// return value : is this sequence 'not' a last sequence?
-bool next_permutation(std::vector<int>& sequence);
+// return value : is this sequence 'not' a first sequence?
+bool prev_permutation(std::vector<int>& sequence);
 void swap(int& a, int& b);
 
 int main(void)
@@ -45,22 +45,22 @@ int main(void)
 		std::cin >> input;
 		seq.push_back(input);
 	}
-	if (!next_permutation(seq)) {std::cout << -1 << '\n'; return 0;}
+	if (!prev_permutation(seq)) {std::cout << -1 << '\n'; return 0;}
 	for(auto i : seq) std::cout << i << ' ';
 	std::cout << '\n';
 	return 0;
 }
-// return value : is this sequence 'not' a last sequence?
-bool next_permutation(std::vector<int>& sequence)
+// return value : is this sequence 'not' a first sequence?
+bool prev_permutation(std::vector<int>& sequence)
 {
 	int i = 0, j = 0;
 	for (i = sequence.size()-1; i >= 0; i--){
-		if (i == 0) return false;		// 전체순열의 마지막 번째
-		if (sequence[i-1] < sequence[i]) break;
+		if (i == 0) return false;		// 전체순열의 첫 번째
+		if (sequence[i-1] >= sequence[i]) break;		// you have to find the first nondecreasing order
 	}
-	// 내림차순의 순열에서 바꿀 수보다 작다는 건 곧 그 이전수가 된다.
+	// find the biggest item smaller than [i-1] item 
 	for (j = i; j < sequence.size()-1; j++)
-		if (sequence[j+1] <= sequence[i-1]) break;
+		if (sequence[j+1] >= sequence[i-1]) break;
 	swap(sequence[i-1], sequence[j]);
 	for (int k = sequence.size()-1; k > i; k--)
 		swap(sequence[i++], sequence[k]);
