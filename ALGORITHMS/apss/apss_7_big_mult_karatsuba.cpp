d0ee13e00e24b37c0972243efa72e39f93c4913c
ChoeSeungHyeon <chltmdgus604@naver.com>
2021-07-07 16:23:11 +0900

    <apss_karatsuba> 구현완료
        z2 값에 미리 10^(half+half) 를 곱해버려서 생긴 문제 하나,
        언제나 an == half + half 일 것이라는 헛된 추측으로 생긴 문제 하나.
    
        왜 an 을 사용하면 안되고 half + half 를 사용해야 하는가?
        a,b가 9자리 수라고 가정하자,
        a = a1 * 10^4 + a0 (이때, a1은 4자리, a0은 5자리)
        b도 마찬가지,
        따라서,
        a * b = a0b0 * 10^8 + (a0b1 + a1b0) * 10^4 + a1b1
        여기에서 10^8 의 8이 곧 half+half를 의미하게 된다.

--- ALGORITHMS/apss/apss_7_big_mult_karatsuba.cpp
+++ ALGORITHMS/apss/apss_7_big_mult_karatsuba.cpp
@@ -6,7 +6,7 @@ extern vector<char> slow_mult(const vector<char>& a, const vector<char>& b);
 // helper functions
 vector<char> operator+(const vector<char>& a, const vector<char>& b);
 vector<char> operator-(const vector<char>& a, const vector<char>& b);
-vector<char>& pow10(vector<char>& a, int amt);
+vector<char> pow10(const vector<char>& a, int amt);
 istream& operator>>(istream& ist, vector<char>& a);
 ostream& operator<<(ostream& os, const vector<char>& a);
 
@@ -14,8 +14,18 @@ int main(void)
 {
     vector<char> a, b;
     cin >> a >> b;
-    cout << a << b;
-    cout << karatsuba(a, b);
+    cout << a << '\n' << b << '\n';
+    cout << karatsuba(a, b) << '\n';
+
+
+    // test +
+    cout << "test operator+(): a+b = " << a+b << '\n';
+    // test -
+    cout << "test operator-(): a-b = " << a-b << '\n';
+    // test pow1
+    cout << "test pow10: pow10(a, 5) = " << pow10(a,5) << '\n';
+
+
     return 0;
 }
 vector<char> karatsuba(vector<char>& a, vector<char>& b)
@@ -27,15 +37,18 @@ vector<char> karatsuba(vector<char>& a, vector<char>& b)
     if (an == 0 || bn == 0) 
         return vector<char>();
     // base condition : a 가 비교적 짧은 경우, 일반 mult로 해결한다.
-    if (an < 5) 
+    if (bn < 5) 
         return slow_mult(a, b);
 
     // a, b를 half 자리와 그 나머지로 분리한다.
     int half = an / 2;
-    vector<char> a0 = vector<char>(a.begin(), a.begin()+half);
-    vector<char> a1 = vector<char>(a.begin()+half, a.end());
-    vector<char> b0 = vector<char>(b.begin(), b.begin() + min(b.size(), (size_t)half));
-    vector<char> b1 = vector<char>(b.begin() + min(b.size(), (size_t)half), b.end());
+    vector<char> a0 {a.begin(), a.begin()+half};
+    vector<char> a1 {a.begin()+half, a.end()};
+    vector<char> b0 {b.begin(), b.begin() + min(b.size(), (size_t)half)};
+    vector<char> b1 {b.begin() + min(b.size(), (size_t)half), b.end()};
+
+    cerr << "a0=" << a0 << ", a1=" << a1 << '\n';
+    cerr << "b0=" << b0 << ", b1=" << b1 << '\n';
 
     // a * b == z0 + z1*pow(10,half) + z2*pow(10, an) 이때,
     // z0 = a0 * b0
@@ -43,12 +56,12 @@ vector<char> karatsuba(vector<char>& a, vector<char>& b)
     // z1 = (a1*b0 + a0*b1) OR ( (a0+a1)*(b0+b1) - z0 - z2 )
     vector<char> z0 = karatsuba(a0, b0);
     vector<char> z2 = karatsuba(a1, b1);
-    z2 = pow10(z2, an);
 
     a0 = a0 + a1;
     b0 = b0 + b1;
     vector<char> z1 = karatsuba(a0, b0) - z0 - z2;
     z1 = pow10(z1, half);
+    z2 = pow10(z2, half+half);
 
     vector<char> ret(0);
     ret = ret + z0 + z1 + z2;
@@ -82,10 +95,11 @@ vector<char> operator-(const vector<char>& a, const vector<char>& b)
     normalize(ret);
     return ret;
 }
-vector<char>& pow10(vector<char>& a, int amt)
+vector<char> pow10(const vector<char>& a, int amt)
 {
-    a.insert(a.begin(), amt, 0);
-    return a;
+    vector<char> ret{a};
+    ret.insert(ret.begin(), amt, 0);
+    return ret;
 }
 // "1234567890" -> [0,9,8,7,6,5,4,3,2,1]
 istream& operator>>(istream& ist, vector<char>& a)
@@ -105,6 +119,5 @@ ostream& operator<<(ostream& os, const vector<char>& a)
     {
         os << (int)a[i] ;
     }
-    os << "\n";
     return os;
 }
