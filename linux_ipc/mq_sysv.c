3afb4ff428f9eb0e663d7c1a2a7168f04b774fbd
ChoiWheatley <chltmdgus604@naver.com>
2021-07-18 10:42:55 +0000

    <linuxipc.sysV.message_queue> SysV 메시지 큐 사용법 터득
        # 개요
            데이터 전송을 위한 SysV api에서 제공하는 IPC 도구. 바이트 스트림 방식과는 다르게 하나의 덩어리를 전송한다고 이해하면 된다.
            Queue를 이용하며, posix 방식 메시지 큐와 차이를 보인다. 기능과 목적이 다르기 때문에 주로 레거시 시스템에서 많이 활용된다.
    
        # 특징
            POSIX 시스템과는 다른점은 하나의 메시지 큐에 여러 리시버 프로세스가 달라붙을 수 있다는 점이다. 프로세스 간 소통이 원활히 이루어 지기 위해선 **message type** 을 일관적으로 설정하여야 다른 프로세스가 메시지 큐를 훔쳐가는 불상사를 막을 수 있다.
    
        # APIs
            - int msgget(key_t key, int msgflg);
            - key_t ftok(const char *pathname, int proj_id);
            - int msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg);
            - ssize_t msgrcv(int msqid, void *msgp, size_t msgz, long msgtype, int msgflg);
            - int msgctl(int msqid, int cmd, struct msqid_ds *buf);
    
            - struct msgbuf {
                long mtype;
                char *mtext;
            };
    
        # Utility
            - ipcs
    
        # 새로 배운 유용한 함수들
            - long int strtol(const char *nptr, char **endptr, int base);
            - int snprintf(char *buffer, size_t n, const char *format_string, argument_list);

---
+++ linux_ipc/mq_sysv.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<assert.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+/*
+//int msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg);
+//
+//ssize_t msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp,
+//        int msgflg);
+*/
+
+
+#define IPC_KEY_FILENAME                "/proc"// 커널이 다루는 프로세스 정보 보관소
+#define IPC_KEY_PROJ_ID                 'a'
+#define IPC_MSG_TYPE                    'a'
+#define MAXBUF_SIZ                      2048
+
+struct msgbuf           // msgsnd, rcv를 위한 msgp
+{
+    long mtype;
+    char buf[MAXBUF_SIZ+1];
+};
+
+static void print_usage(const char *progname)
+{
+    printf("%s <send | recv> <message_type>\n", progname);
+}
+static int init_msgq(void)
+{
+    int msgq;
+    key_t key;
+    if ((key = ftok(IPC_KEY_FILENAME, IPC_KEY_PROJ_ID)) == -1 )
+    {
+        perror("ftok\n");
+        return -1;
+    }
+    if((msgq = msgget(key, 0644 | IPC_CREAT)) == -1 ) 
+    {
+        perror("msgget\n");
+        return -1;
+    }
+    return msgq;
+}
+
+static int do_send(long mtype)
+{
+    int msgq;
+    struct msgbuf mbuf;
+    if (mtype <= 0)
+    {
+        perror("mtype is smaller or equal than 0\n");
+        return -1;
+    }
+
+    assert( (msgq = init_msgq()) != -1 );
+
+//strncpy(mbuf.buf, "hello world!", sizeof(mbuf.buf));
+//      int snprintf(char *str, size_t size, const char *format, ...);
+    snprintf(mbuf.buf, sizeof(mbuf.buf), "hello world!<%ld>", mtype);
+    mbuf.mtype=mtype;           // message type 설정
+//      int msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg);
+    if (msgsnd(msgq, &mbuf, sizeof(mbuf.buf), IPC_KEY_PROJ_ID) == -1) 
+    {
+        perror("msgsnd\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int do_recv(long mtype)
+{
+    int msgq;
+    struct msgbuf mbuf;
+    int ret;
+
+    assert( (msgq = init_msgq()) != -1 );
+
+    memset( &mbuf, 0, sizeof(mbuf) );
+//ssize_t msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp,
+//               int msgflg);
+    if ( (ret = msgrcv(msgq, &mbuf, sizeof(mbuf.buf), mtype, 0)) == -1)
+    {
+        perror("msgrcv\n");
+        return -1;
+    }
+    printf("received msg: mtype:%ld, msg:[%s]\n", mbuf.mtype, mbuf.buf);
+
+    return 0;
+}
+
+int main(int argc, char ** argv)
+{
+    int ret;
+    long mtype;
+    if (argc < 3) 
+    {
+        goto throw;
+    }
+
+//long int strtol(const char *nptr, char **endptr, int base);
+    mtype = strtol(argv[2], NULL, 10);
+    if (!strcmp(argv[1], "send"))
+    {
+        ret = do_send(mtype);
+    } 
+    else if (!strcmp(argv[1], "recv"))
+    {
+        ret = do_recv(mtype);
+    }
+    else
+    {
+        goto throw;
+    }
+
+    return ret;
+
+throw:
+    print_usage(argv[0]);
+    return -1;
+}
