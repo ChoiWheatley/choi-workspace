ab08c003acd7bdc8db5c3befd005c1e9c401b127
ChoiWheatley <chltmdgus604@naver.com>
2021-07-24 07:00:57 +0000

    <APSS.8.jumpgame> 왜 틀리는 건지 모르겠다니까

--- ALGORITHMS/apss/8_jumpgame.cpp
+++ ALGORITHMS/apss/8_jumpgame.cpp
@@ -4,13 +4,14 @@
 #include<iostream>
 #include<cstring>
 
-#define MAXN 100
+#define MAXN 101
 
 int memo[MAXN][MAXN] = {-1};
 int board[MAXN][MAXN] = {0};
 
-void input(int board[MAXN][MAXN], int& n);
+void input(int& n);
 int jumpgame(const int y, const int x, const int n);
+void print(const int[MAXN][MAXN], int len);
 
 int main(int argc, char ** argv)
 {
@@ -18,7 +19,11 @@ int main(int argc, char ** argv)
     std::cin >> c;
     while(c-->0)
     {
-        input(board, n);
+        input(n);
+#if DBG == 0
+        std::cout << "board:\n";
+        print(board, n);
+#endif
         
         std::cout << (jumpgame(0, 0, n) ? "YES" : "NO") << '\n';
     }
@@ -28,6 +33,7 @@ void input(int& n)
 {
     memset(board, 0, MAXN*MAXN);
     memset(memo, -1, MAXN*MAXN);
+
     std::cin >> n;
     for (int i = 0; i < n; i++)
     {
@@ -48,8 +54,16 @@ int jumpgame(const int y, const int x, const int n)
     if ( ref!=-1 )
         return ref;
     int jump=board[y][x];
-    return (
-        ref = jumpgame(y+jump, x, n) ||
-        jumpgame(y, x+jump, n)
-    );
+    ref =   jumpgame(y+jump,    x,      n) ||
+            jumpgame(y,         x+jump, n);
+    return ref;
+}
+void print(const int arr[MAXN][MAXN], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+            std::cerr << arr[i][j] << ' ';
+        std::cerr << '\n';
+    }
 }
