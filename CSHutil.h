54eca8824835f08696fb78b8d08921fe7780a383
ChoiWheatley <chltmdgus604@naver.com>
2021-07-31 02:08:44 +0000

    <CSHutil> 나만의 유용한 헤더파일 작성 시작
    라이브러리로 확장해 나갈 수 있을까? 유용한 기능을 담고 적재적소에 써먹을 수만 있다면..!
    
     - [ ] C style library ( helper functions, macro functions, c style algorithms, c style data structures, library interpositioning을 이용한 유용한 래핑 함수들 )
     - [ ] C++ style library ( helper class, exception handler, own algorithms, own data structures )

---
+++ CSHutil.h
@@ -0,0 +1,21 @@
+/*
+ *  이 헤더파일은 C, C++ 코딩을 하며 즉석으로 제작한 유용한 [ 헬퍼함수 / 매크로 / 오류처리 / IO ]  관련 코드를 아카이빙 합니다. C 스타일 헤더파일이 될 것이고, 만일 C++ 에서만 작동하는 기능이 있다면 c++를 위한 전용 라이브러리를 하나 새로 팔 예정입니다.
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<assert.h>
+
+ 
+// INSPECT() 매크로 함수는 assert() 매크로의 확장판입니다. __func__, __LINE__, #expr 까지는 동일한데, 에러 핸들링 과정을 추가했습니다.  
+// 용법: INSPECT( <expr_a> , <expr_b> )
+// expr_a 의 값이 assert의 것과 동일하게 'false'이면  expr_b를 실행합니다.
+#ifndef INSPECT
+#define INSPECT(expr, then) \
+    if ((expr)==0) {\
+        fprintf(stderr, "in function(%s), line number(%d),\n", __func__, __LINE__);\
+        fprintf(stderr, "`%s` has error occured!\n", #expr);\
+        then\
+    }
+#endif//INSPECT
