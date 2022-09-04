9e13bb02515c2f074058bc22dd8f59a800583ae1
ChoiWheatley <chltmdgus604@naver.com>
2022-09-04 17:21:47 +0900

    test fixture 수정

--- ALGORITHMS/programmers/신고결과받기.cpp
+++ ALGORITHMS/programmers/신고결과받기.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-vector<int> solution(vector<string> id_list, vector<string> report, int k)
+static vector<int> solution(vector<string> id_list, vector<string> report, int k)
 {
   vector<int> answer;
   return answer;
@@ -50,6 +50,7 @@ namespace testing
         auto input = istringstream{each};
         for (string word; std::getline(input, word, ' ');)
         {
+          ASSERT_NE(id_set.find(word), id_set.end());
           words.insert(word);
         }
         ASSERT_EQ(words.size(), 2) << "Each report must have exactly two words!";
@@ -58,10 +59,22 @@ namespace testing
       ASSERT_GE(k, 1);
       ASSERT_LE(k, 200);
 
+      ASSERT_EQ(result.size(), id_list.size());
+
       auto const answer = solution(id_list, report, k);
       EXPECT_EQ(result, answer);
     }
   }; // class Fixture
+
+  // TEST_F(ReportFixture, WrongTestCase)
+  // {
+  //   id_list = {"a", "b", "c"};
+  //   report = {"a d"};
+  //   k = 3;
+  //   result = {0, 0, 0};
+
+  //   DoTest();
+  // }
 
   TEST_F(ReportFixture, 1)
   {
@@ -69,6 +82,17 @@ namespace testing
     report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
     k = 2;
     result = {2, 1, 1, 0};
+
+    DoTest();
+  }
+
+  /// ryan이 con을 4번 신고했으나 한 유저가 같은 유저를 여러 번 신고한 경우는 1회로 처리한다.
+  TEST_F(ReportFixture, 2)
+  {
+    id_list = {"con", "ryan"};
+    report = {"ryan con", "ryan con", "ryan con", "ryan con"};
+    k = 3;
+    result = {0, 0};
 
     DoTest();
   }
