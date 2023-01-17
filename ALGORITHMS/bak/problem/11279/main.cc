99df93ed29167d197d45a2b29c56884f9c350671
ChoiWheatley <chltmdgus604@naver.com>
2023-01-17 20:22:07 +0900

    [11279] 최대힙 구현 - 존나 어렵네

--- ALGORITHMS/bak/problem/11279/main.cc
+++ ALGORITHMS/bak/problem/11279/main.cc
@@ -3,5 +3,21 @@
 
 int main(void)
 {
-    std::cout << "hello\n";
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int n;
+    std::cin >> n;
+    MaxHeap heap{};
+    while (n-- > 0)
+    {
+        int val;
+        std::cin >> val;
+        if (val == 0)
+        {
+            std::cout << heap.pop_max() << "\n";
+        }
+        heap.add(val);
+    }
 }
--- ALGORITHMS/bak/problem/11279/solution.hpp
+++ ALGORITHMS/bak/problem/11279/solution.hpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <vector>
 
 /**
@@ -5,34 +6,76 @@
  */
 class MaxHeap
 {
-    const int N;
     std::vector<int> heap;
 
 public:
-    explicit MaxHeap(int n = 100) : N{n * 2}, heap{n}
-    {
-    }
-
-    void add(int val)
+    MaxHeap &add(int val)
     {
         heap.push_back(val);
 
-        size_t pos = heap.size() - 1;
-        size_t parent_pos = pos / 2;
-        while (pos > 0 && heap.at(parent_pos) < val)
+        std::size_t pos = heap.size() - 1;
+        std::size_t parent_pos = (pos - 1) / 2;
+        while (pos > 0 && heap[pos] > heap[parent_pos])
         {
             std::swap(heap[pos], heap[parent_pos]);
             pos = parent_pos;
-            parent_pos /= 2;
+            parent_pos = (pos - 1) / 2;
         }
+
+        return *(this);
     }
 
-    auto get_max() -> int
+    bool is_empty()
+    {
+        return heap.empty();
+    }
+
+    auto get_max() const -> int
     {
         if (heap.empty())
         {
             return 0;
         }
         return heap.at(0);
+    }
+
+    /**
+     * 1. root를 제거하고 다음 원소를 올려놓는다.
+     * 2. 만약 힙의 속성을 위배한 경우 그 다음 루트의 아래로 sink down 하게 된다.
+     */
+    auto pop_max() -> int
+    {
+        if (heap.empty())
+        {
+            return 0;
+        }
+
+        int max = heap[0];
+
+        // pop last object and set to root
+        int last = heap.back();
+        heap.erase(heap.end() - 1);
+        heap[0] = last;
+
+        // sink down last element
+        std::size_t child_idx = 0;
+        for (std::size_t idx = 0; idx < heap.size(); idx = child_idx)
+        {
+            child_idx = idx * 2 + 1;
+            if (child_idx + 1 < heap.size() && heap.at(child_idx + 1) > heap.at(child_idx))
+            {
+                child_idx++; // left child VS right child
+            }
+            if (child_idx < heap.size() && last < heap.at(child_idx))
+            {
+                std::swap(heap[idx], heap[child_idx]);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return max;
     }
 };
